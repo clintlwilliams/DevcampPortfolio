fecaf7c3d4d22d6ba697f9dbe39275d136c6fac2
Clint L Williams <cwilliams@gracehill.com>
2020-04-17 15:39:31 -0400

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "CW Ruby Portfolio | My Portfolio Website"
     @seo_keywords = "Clint Williams' portfolio"
   end
 end
